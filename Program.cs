8b5ccfbe98a1cb7536aa81b89103963e2b930bf9
jigarhirani <hirani.jigar117@gmail.com>
2024-03-22 12:20:28 +0530

    Bug Fixes & Layout Changes
    _Layout File changes with partial views

--- Program.cs
+++ Program.cs
@@ -12,10 +12,12 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//}
+app.UseExceptionHandler("/Home/Error");
+
 app.UseStaticFiles();
 
 app.UseRouting();
